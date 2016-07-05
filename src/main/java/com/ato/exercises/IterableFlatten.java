c4d556ec9be6a67605c1e26b49e9d9120cd107ff
Tu Tran <tranduongtu92@gmail.com>
2016-07-05 15:13:31 +0700

    Initial commit

---
+++ src/main/java/com/ato/exercises/IterableFlatten.java
@@ -0,0 +1,11 @@
+package com.ato.exercises;
+
+/**
+ * Created by tutran on 7/5/16.
+ */
+public final class IterableFlatten {
+
+  public static <T> Iterable<T> flatten(final Iterable<? extends Iterable<T>> input) {
+    return null;
+  }
+}
---
+++ src/test/java/com/ato/exercises/IterableFlattenTest.java
@@ -0,0 +1,34 @@
+package com.ato.exercises;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * Created by tutran on 7/5/16.
+ */
+public class IterableFlattenTest {
+
+  @Test
+  public void testFlattenSimpleLists() {
+    final List<List<Integer>> input = new ArrayList<>();
+    input.add(Arrays.asList(1, 2, 3));
+    input.add(Collections.<Integer> emptyList());
+    input.add(Arrays.asList(4, 5, 6));
+    final Iterable<Integer> result = IterableFlatten.flatten(input);
+
+    final Iterator<Integer> iter = result.iterator();
+    final List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+    final List<Integer> actual = new ArrayList<>();
+    while (iter.hasNext()) {
+      actual.add(iter.next());
+    }
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testFlattenNestedLists() {
+
+  }
+}
